1f4e13bf1039bba298002cb2b7479bf51e5622a4
joy <joypal.hazard@gmail.com>
2024-09-11 20:08:33 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const addBtn = document.querySelector('#add');
+const textArea = document.querySelector('.text-area textarea');
+const notes = document.querySelector('.notes');
+const colorInput = document.querySelector('#color');
+const sizeInput = document.querySelector('#size');
+
+//Adding Notes by clicking the Add button
+addBtn.addEventListener('click', (e)=>{
+    if(textArea.value === ''){
+        alert('Please Enter a notes.');
+        box.remove();
+    }
+
+    const box = document.createElement('div');
+    box.className = 'box';
+    const text = document.createElement('p');
+    const closeBtn = document.createElement('button');
+    notes.appendChild(box);
+    box.appendChild(text);
+    box.appendChild(closeBtn);
+    closeBtn.innerHTML = 'X';
+    closeBtn.addEventListener('click', ()=>{
+        box.remove();
+    })
+    text.innerHTML = textArea.value;
+    text.style.color = colorInput.value;
+    text.style.fontsize = sizeInput.value + 'px';
+    if(textArea.value === text.innerHTML){
+        textArea.value = '';
+    }
+})
+
+//Change the color of the text
+colorInput.addEventListener('change', ()=>{
+    textArea.style.color = colorInput.value;
+})
+
+//changing the font size of the text
+sizeInput.addEventListener('change', ()=>{
+    textArea.style.fontsize = sizeInput.value + 'px';
+})
+
